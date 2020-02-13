16dafd6fb4d1ca542c294769cbb023d1d90a7ee3
Glitch (code-2) <none>
2020-02-13 08:24:22 +0000

    🎲🏟 Checkpoint ./p2.html:749274/111934 ./script.js:749274/93 ./style.css:749274/18

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("toggle");
+  var x = document.getElementById("toggle", "toggle1");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
